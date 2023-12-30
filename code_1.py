91ed8368a2c2f290afe5b634e253c30042053f92
vyrguskinys <144053476+vyrguskinys@users.noreply.github.com>
2023-12-30 23:26:08 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,25 @@
+
+        int[] data = new int[10];
+    }
+        }
+public class RandomDataGenerator {
+}
+    public static void main(String[] args) {
+        for (int item : data) {
+            System.out.println("Random Number: " + item);
+        for (int i = 0; i < 10; i++) {
+        }
+
+            data[i] = (int) (Math.random() * 100) + 1;    public static void main(String[] args) {
+    }
+public class RandomDataGenerator {
+
+        for (int i = 0; i < 10; i++) {
+        for (int item : data) {
+}
+        }
+        }
+
+        int[] data = new int[10];
+            data[i] = (int) (Math.random() * 100) + 1;
+            System.out.println("Random Number: " + item);
